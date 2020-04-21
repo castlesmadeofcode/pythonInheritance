06b417b53b08b1bb50e1d7a13f3260d1b0498bd5
castle <castlesmadeofcode@gmail.com>
2020-04-20 21:23:25 -0500

    initial commit

---
+++ colorsAndSounds.py
@@ -0,0 +1,23 @@
+# Define 5 of your favorite vehicles
+
+# Move all common properties in your vehicles to a new Vehicle class.
+
+# Create an instance of each vehicle.
+
+# Define a different value for each vehicle's properties.
+
+# Create a drive() method in the Vehicle class.
+
+# Override the drive() method in all the other vehicle classes. 
+# Include the vehicle's color in the message (i.e. "The blue Ram drives past.
+#  RRrrrrrummbbble!").
+
+# Create a turn(self, direction) method, and a stop(self) method on Vehicle. 
+# Define a basic implementation of each.
+
+# Override all three of those methods on some of the vehicles.
+#  For example, the stop() method for a plane would be to output the message
+# "The white Cessna rolls to a stop after rolling a mile down the runway."
+
+
+# Make your vehicle instances perform all three behaviors.
